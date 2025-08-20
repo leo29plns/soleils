15ee3a0c4c53e9c4e52662e5ab3d77db4cafc576
leo-planus-ext_sonepar <leo.planus_ext@sonepar.com>
2025-08-20 22:57:56 +0200

    Added biome

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-
 import svelte from '@astrojs/svelte';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://soleils.leo29plns.fr',
-    integrations: [mdx(), sitemap(), svelte()],
+	site: 'https://soleils.leo29plns.fr',
+	integrations: [mdx(), sitemap(), svelte()],
 });
