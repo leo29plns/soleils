181bdc07f0357df42c89260b0924cc8d0cecbb26
leo-planus-ext_sonepar <leo.planus_ext@sonepar.com>
2025-08-20 23:44:08 +0200

    Removed mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import { defineConfig } from 'astro/config';
@@ -8,5 +7,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://soleils.leo29plns.fr',
-	integrations: [mdx(), sitemap(), svelte()],
+	integrations: [sitemap(), svelte()],
 });
