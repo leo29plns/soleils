9f97d06b5bb761e90cecf98e685e8e34ebf7e2a2
leo-planus-ext_sonepar <leo.planus_ext@sonepar.com>
2025-08-21 08:39:54 +0200

    client routing

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Soleils ☀️';
+export const SITE_TITLE = 'Soleils';
 export const SITE_DESCRIPTION =
 	'Pour toutes ces petites histoires qui font chaud au cœur.';
